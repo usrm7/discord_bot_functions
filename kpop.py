7791024d1081c6bcc8fbd0bd6e48262b16044af7
usrm7 <85717140+usrm7@users.noreply.github.com>
2021-06-18 13:21:39 -0600

    Update kpop.py
    changed formatting and some variables to conform to PEP

--- kpop.py
+++ kpop.py
@@ -1,3 +1,9 @@
+import random
+import urllib.request
+import json
+import os
+
+
 def get_random_kpop_video():
     """This function returns a YouTube URL for a random kpop video"""
 
@@ -7,19 +13,19 @@ def get_random_kpop_video():
     search_term = 'kpop+new'
     video_ids = []
 
-    urlData = "https://www.googleapis.com/youtube/v3/search?key={}&maxResults={}&part=snippet&type=video&q={}".format(
+    url_data = "https://www.googleapis.com/youtube/v3/search?key={}&maxResults={}&part=snippet&type=video&q={}".format(
         API_KEY, count, search_term)
 
-    webURL = urllib.request.urlopen(urlData)
-    data = webURL.read()
-    encoding = webURL.info().get_content_charset('utf-8')
+    web_url = urllib.request.urlopen(url_data)
+    data = web_url.read()
+    encoding = web_url.info().get_content_charset('utf-8')
     results = json.loads(data.decode(encoding))
 
     for data in results['items']:
-        videoId = (data['id']['videoId'])
-        #print(videoId)
-        video_ids.append(videoId)
+        video_id = (data['id']['video_id'])
+        # print(video_id)
+        video_ids.append(video_id)
 
     video_url = "https://youtu.be/" + random.choice(video_ids)
-    #print(video_url)
+    # print(video_url)
     return video_url
